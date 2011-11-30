c8692b2c0412356f3e63d0114c3117e54f90fa90
Sergey Astanin <s.astanin@gmail.com>
2011-11-30 15:59:03 +0100

    Single example for all Rosenbrock problem solutions.
    Algorithm operators (GM + BLX-α/UNDX/SBX) can be chosen at runtime.

---
+++ examples/rosenbrock.hs
@@ -0,0 +1,91 @@
+{- Minimize Rosenbrock function using real-valued genetic algorithm.
+   Optimal value x* = (1,...,1). F(x*) = 0.
+-}
+
+import AI.SimpleEA
+import AI.SimpleEA.Utils
+import AI.SimpleEA.Rand
+import Control.Monad
+import Data.List
+import Data.Ord (comparing)
+import Print (printHistoryAndBest)
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(..))
+
+rosenbrock :: [Double] -> Double
+rosenbrock xs = sum . map f $ zip xs (drop 1 xs)
+  where
+   f (x1, x2) = 100 * (x2 - x1^2)^2 + (x1 - 1)^2
+
+nvariables = 2
+xrange = (-30.0, 30.0)
+popsize = 100
+precision = 1e-4
+maxiters = 5000 :: Int
+
+-- fitness function is maximized  when Rosenbrock function is minimized
+fitness xs _ = negate $ rosenbrock xs
+
+-- selection: tournament selection with elitism
+select pop =
+    let keep = popsize `div` 10
+        top = take keep (elite pop)
+    in  do
+      rest <- tournamentSelect 3 (popsize - keep) pop
+      return (top ++ rest)
+
+-- Gaussian mutation
+gm =
+    let p = 0.5/fromIntegral nvariables
+        s = 0.1*(snd xrange - fst xrange)
+    in  gaussianMutate p s
+
+mutationOps = [ ("gm", gm) ]
+
+-- BLX-0.5 crossover
+blxa = blendCrossover 0.5
+-- UNDX crossover
+undx = unimodalCrossoverRP
+-- SBX crossover
+sbx = simulatedBinaryCrossover 2
+
+crossoverOps = [ ("blxa", blxa), ("undx", undx), ("sbx", sbx) ]
+
+printUsage = do
+  putStrLn usage
+  exitWith (ExitFailure 1)
+  where
+  usage = intercalate " " [ "rosenbrock", mops, xops ]
+  mops = intercalate "|" (map fst mutationOps)
+  xops = intercalate "|" (map fst crossoverOps)
+
+-- digest: what to log on every iteration
+digest pop =
+  let m = maxFitness pop
+      a = avgFitness pop
+  in  (a, m)
+
+geneticAlgorithm mutate crossover = do
+  -- initial population
+  genomes0 <- replicateM popsize $ replicateM nvariables (getRandomR xrange)
+  let pop0 = evalFitness fitness genomes0
+  -- run genetic algorithm
+  loopUntil' (MaxFitness (>= (-precision))
+              `Or` Iteration maxiters) digest pop0 $
+              nextGeneration fitness select crossover mutate
+
+-- usage: rosenbrock mutationOperator crossoverOperator
+main = do
+  args <- getArgs
+  conf <- case args of
+           (m:x:[]) -> return (lookup m mutationOps, lookup x crossoverOps)
+           _        -> printUsage
+  case conf of
+    (Just mutate, Just crossover) -> do
+       (pop, log) <- runGA $ geneticAlgorithm mutate crossover
+       printHistoryAndBest show pop log
+       let bestF = snd . head . sortBy (comparing (negate . snd)) $ pop
+       if (bestF >= (-precision))
+          then exitWith ExitSuccess
+          else exitWith (ExitFailure 2)  -- failed to find a solution
+    _ -> printUsage
