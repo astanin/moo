5f4f0fe331a611dc99c518aabfd8129065fae6c7
Sergey Astanin <s.astanin@gmail.com>
2013-04-03 17:57:09 +0200

    Beale example in the general description

--- Moo/GeneticAlgorithm.hs
+++ Moo/GeneticAlgorithm.hs
@@ -121,7 +121,35 @@ Examples
 --------
 @
 
-See @examples/@ folder of the source distribution.
+Minimizing Beale's function:
+
+@
+import Moo.GeneticAlgorithm.Continuous
+
+
+beale :: [Double] -> Double
+beale [x, y] = (1.5 - x + x*y)**2 + (2.25 - x + x*y*y)**2 + (2.625 - x + x*y*y*y)**2
+
+
+popsize = 101
+elitesize = 1
+tolerance = 1e-6
+
+
+selection = tournamentSelect Minimizing 2 (popsize - elitesize)
+crossover = unimodalCrossoverRP
+mutation = gaussianMutate 0.25 0.1
+step = nextGeneration Minimizing beale selection elitesize crossover mutation
+stop = IfObjective (\\values -> (minimum values) < tolerance)
+initialize = getRandomGenomes popsize 2 (-4.5, 4.5)
+
+
+main = do
+  population <- runGA initialize (loop stop step)
+  print (head . bestFirst Minimizing $ population)
+@
+
+See @examples/@ folder of the source distribution for more examples.
 
 -}
 
