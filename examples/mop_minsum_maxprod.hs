fb89fe48e589dbe17090a55eba5977dc43fd2275
Sergey Astanin <s.astanin@gmail.com>
2013-04-11 18:51:11 +0200

    update mop_minsum_maxprod example

--- examples/mop_minsum_maxprod.hs
+++ examples/mop_minsum_maxprod.hs
@@ -5,6 +5,7 @@ import Moo.GeneticAlgorithm.Multiobjective
 
 
 import System.Environment (getArgs)
+import Text.Printf (printf)
 
 
 mop :: MultiObjectiveProblem ([Double] -> Double)
@@ -24,9 +25,16 @@ main = do
   args <- getArgs
   let args' = zip (iterate (+ 1) 0) args
   let n = maybe (length genomes) read $ lookup (0::Int) args'
-  putStrLn $ show n ++ " genomes"
+  putStrLn $ "# population size: " ++ show n
   result <- runGA
             (return . take n . cycle $ genomes) $
             (loop (Generations 100) step)
-  putStrLn $ "best:"
-  mapM_ print (mape fst . take 10 $ result)
+  putStrLn $ "# best:"
+  printPareto result
+
+
+printPareto result = do
+  let paretoGenomes = map takeGenome . takeWhile ((== 1.0) . takeObjectiveValue) $ result
+  let paretoObjectives = map takeObjectiveValues $ evalAllObjectives mop paretoGenomes
+  putStr $ unlines $
+       map (\[x,y] -> printf "%12.3f\t%12.3f" x y ) paretoObjectives
