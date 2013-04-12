89abf7092a26bead19cfac0a8d5168c68804cf2f
Sergey Astanin <s.astanin@gmail.com>
2013-04-12 18:15:01 +0200

    constrainedTournament

--- Moo/GeneticAlgorithm/Constraints.hs
+++ Moo/GeneticAlgorithm/Constraints.hs
@@ -1,14 +1,21 @@
 module Moo.GeneticAlgorithm.Constraints
     ( ConstraintFunction
     , Constraint(..), greaterThan, greaterThanOrEqual
-    , satisfiesConstraint
     , isFeasible
     , numberOfViolations
     , degreeOfViolation
+    , constrainedTournament
     ) where
 
 
 import Moo.GeneticAlgorithm.Types
+import Moo.GeneticAlgorithm.Random
+
+
+import Control.Arrow (first)
+import Control.Monad (replicateM)
+import Data.List (sortBy)
+import Data.Function (on)
 
 
 type ConstraintFunction a b = Genome a -> b
@@ -49,7 +56,7 @@ greaterThanOrEqual f v = LessThanOrEqual (negate . f) (negate v)
 
 -- | Returns @True@ if a @genome@ represents a feasible solution
 -- with respect to the @constraint@.
-satisfiesConstraint :: (Num b, Ord b)
+satisfiesConstraint :: (Real b)
           => Genome a        -- ^ @genome@
           -> Constraint a b  -- ^ @constraint@
           -> Bool
@@ -60,44 +67,89 @@ satisfiesConstraint g (Equal f v) = f g == v
 
 -- | Returns @True@ if a @genome@ represents a feasible solution,
 -- i.e. satisfies all @constraints@.
-isFeasible :: (Num b, Ord b)
-           => Genome a          -- ^ @genome@
-           -> [Constraint a b]  -- ^ @constraints@
+isFeasible :: (Real b)
+           => Genome a          -- ^ genome
+           -> [Constraint a b]  -- ^ constraints
            -> Bool
 isFeasible genome constraints = all (genome `satisfiesConstraint`) constraints
 
 
--- | Count the number of constraint violations. The result can be used
--- as a degree of feasibility to compare unfeasible solutions.
--- Returns @0@ if the solution is feasible.
-numberOfViolations :: (Num b, Ord b) => [Constraint a b] -> Genome a -> Int
+-- | A simple estimate of the degree of feasibility.
+--
+-- Count the number of constraint violations. Return @0@ if the solution is feasible.
+numberOfViolations :: (Real b)
+                   => [Constraint a b]  -- ^ constraints
+                   -> Genome a  -- ^ genome
+                   -> Int  -- ^ the number of violated constraints
 numberOfViolations constraints genome =
     let satisfied = map (genome `satisfiesConstraint`) constraints
     in  length $ filter not satisfied
 
 
--- | A quantitative non-negative estimate of the degree of feasibility.
+-- | An estimate of the degree of feasibility.
 --
--- Given @f_j@ is an excess of @j@-th constraint function value, return
--- @sum |f_j|^beta@. Return @0.0@ if the solution is feasible.
--- For non-strict inequality constraints, return @sum |f_j + 1.0|^beta@.
+-- Given @f_j@ is the excess of @j@-th constraint function value,
+-- return @sum |f_j|^beta@.  For strict inequality constraints, return
+-- @sum (|f_j|^beta + eta)@.  Return @0.0@ if the solution is
+-- feasible.
 --
-degreeOfViolation :: Double -> [Constraint a Double] -> Genome a -> Double
-degreeOfViolation beta constraints genome =
+degreeOfViolation :: Double  -- ^ beta, single violation exponent
+                  -> Double  -- ^ eta, equality penalty in strict inequalities
+                  -> [Constraint a Double] -- ^ constrains
+                  -> Genome a  -- ^ genome
+                  -> Double    -- ^ total degree of violation
+degreeOfViolation beta eta constraints genome =
     sum $ map violation constraints
   where
     violation (LessThan f v) =
         let v' = f genome
         in  if v' < v
             then 0.0
-            else (abs $ v' - v) ** beta
+            else (abs $ v' - v) ** beta + eta
     violation (LessThanOrEqual f v) =
         let v' = f genome
         in  if v' <= v
             then 0.0
-            else (abs $ 1.0 + v' - v) ** beta
+            else (abs $ v' - v) ** beta
     violation (Equal f v) =
         let v' = f genome
         in  if v' == v
             then 0.0
             else (abs $ v' - v) ** beta
+
+
+-- | Constrained tournament selection prefers feasible solutions over infeasible,
+-- solutions with small constraint violations over other infeasible solutions,
+-- and is equivalent to normal tournament if all solutions are feasible.
+-- Runs @n@ tournaments in groups of size @size@.
+constrainedTournament :: (Real b, Real c)
+    => [Constraint a b]  -- ^ constraints
+    -> ([Constraint a b] -> Genome a -> c)  -- ^ degree of violation function
+    -> ProblemType  -- ^ type of the optimization problem
+    -> Int -- ^ @size@ of the tournament group
+    -> Int -- ^ @n@, how many tournaments to run
+    -> SelectionOp a
+constrainedTournament constraints violation ptype size n xs =
+    replicateM n tournament1
+  where
+  xs_ds =
+      let ds = map (violation constraints . takeGenome) xs
+      in  zip xs ds
+  cmp = (constrainedCompare ptype) `on` (first takeObjectiveValue)
+  tournament1 = do
+    contestants <- randomSample size xs_ds
+    let winner = fst . head $ sortBy cmp contestants
+    return winner
+
+
+constrainedCompare :: (Real c) => ProblemType -> (Objective, c) -> (Objective, c) -> Ordering
+constrainedCompare ptype (objval1, violation1) (objval2, violation2) =
+    let infeasible1 = violation1 > 0
+        infeasible2 = violation2 > 0
+    in  case (infeasible1, infeasible2) of
+          (True, True) -> compare violation1 violation2  -- prefer smaller violation
+          (False, False) -> case ptype of
+                              Minimizing -> compare objval1 objval2
+                              Maximizing -> compare objval2 objval1
+          (True, False) -> GT  -- second (feasible) solution is preferred
+          (False, True) -> LT  -- first (feasible) solution is preferred
