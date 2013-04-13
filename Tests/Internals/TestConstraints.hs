1a937d8d8f0577349536cb079c3899248191a56a
Sergey Astanin <s.astanin@gmail.com>
2013-04-13 23:49:54 +0200

    tests for constrainedTournament

--- Tests/Internals/TestConstraints.hs
+++ Tests/Internals/TestConstraints.hs
@@ -29,4 +29,17 @@ testConstraints =
                     all (\([_,y]) -> (-1) <= y && y <= 1) genomes
         assertEqual "third constraint (<)" True $
                     all (\([x,y]) -> (x+y) < 5) genomes
+    , "constrained tournament" ~: do
+        let tournament = constrainedTournament [head .>=. 0, head .>=. (-1)]
+                         numberOfViolations Minimizing 2 100
+        -- out of two solutions, one violates both constraints, another one only one
+        let badvsugly = map (\x -> ([x], x)) [-1, -2]
+        -- out of two solutions, one is feasible, the other is not
+        let goodvsbad = map (\x -> ([x], x)) [0, -1]
+        let result = flip evalRandom (pureMT 1) $ tournament badvsugly
+        assertEqual "lesser degree of violation is preferred"
+                    (replicate 100 (-1.0)) $ (map snd result)
+        let result = flip evalRandom (pureMT 1) $ tournament goodvsbad
+        assertEqual "feasible solution is preferred"
+                    (replicate 100 (0.0)) $ (map snd result)
     ]
