765216b7065be3a246351083d8de8a13fd47bd9d
Sergey Astanin <s.astanin@gmail.com>
2013-04-19 00:19:29 +0200

    example: CONSTR2 problem from Deb 2002; doesn't work yet

---
+++ examples/mop_constr2.hs
@@ -0,0 +1,47 @@
+{- CONSTR2 problem from (Deb. 2002).
+   A part of the unconstrained Pareto-optimal region is not feasible.
+-}
+
+
+import Moo.GeneticAlgorithm.Continuous
+import Moo.GeneticAlgorithm.Constraints
+import Moo.GeneticAlgorithm.Multiobjective
+
+
+popsize = 100
+generations = 100
+
+
+mop :: MultiObjectiveProblem ([Double] -> Double)
+mop = [ (Minimizing, \[x1,_] -> x1)
+      , (Minimizing, \[x1,x2] -> (1+x2)/x1) ]
+
+
+constraints = [ 0.1 .<= x1 <=. 1.0
+              , 0.0 .<= x2 <=. 5.0
+              , g1 .>=. 6.0
+              , g2 .>=. 1.0 ]
+  where
+    x1 [x,_] = x
+    x2 [_,y] = y
+    g1 [x1,x2] = 9*x1 + x2
+    g2 [x1,x2] = 9*x1 - x2
+
+
+
+initialize = getConstrainedGenomesRs constraints popsize [(0.1,1.0),(0.0,5.0)]
+tournament = withConstraints constraints numberOfViolations Minimizing $
+             tournamentSelect Minimizing 2 popsize
+
+
+step :: StepGA Rand Double
+step = withFinalDeathPenalty constraints $
+       stepNSGA2 mop tournament (blendCrossover 0.1) noMutation -- (gaussianMutate 0.5 0.5)
+
+
+main = do
+  result <- runGA initialize $ loop (Generations generations) step
+  let solutions = map takeGenome $ takeWhile ((<= 10.0) . takeObjectiveValue) result
+  let ovs = map takeObjectiveValues $ evalAllObjectives mop solutions
+  flip mapM_ ovs $ \[x1,x2] ->
+      putStrLn $ show x1 ++ "\t" ++ show x2
