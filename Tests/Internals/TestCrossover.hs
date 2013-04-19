7e2efb41bee8490ad76b5c75ebed0bed11072172
Sergey Astanin <s.astanin@gmail.com>
2013-04-19 16:38:50 +0200

    tests for discrete crossover operators

--- Tests/Internals/TestCrossover.hs
+++ Tests/Internals/TestCrossover.hs
@@ -3,6 +3,7 @@ module Tests.Internals.TestCrossover where
 
 import Test.HUnit
 import System.Random.Mersenne.Pure64 (pureMT)
+import Data.List (group)
 
 
 import Moo.GeneticAlgorithm.Types
@@ -35,4 +36,48 @@ testCrossover =
                       \i -> flip evalRandom (pureMT i) $
                       doCrossovers genomes (onePointCrossover 1.0)
         assertEqual "discard last genomes without a pair" [] result3
+    , "simple crossover" ~: do
+        let ones = replicate 8 1
+        let zeros = replicate 8 0
+        let genomes = [ones, zeros]
+        let n = 1000
+        assertEqual "exactly one crossover point" True $
+                    all (<=2) . map (length . group) $
+                        flip evalRandom (pureMT 1) (doNCrossovers n genomes (onePointCrossover 1))
+    , "simple crossover" ~: do
+        let ones = replicate 8 1
+        let zeros = replicate 8 0
+        let genomes = [ones, zeros]
+        let n = 1000
+        assertEqual "exactly one crossover point" True $
+                    all (<=2) . map (length . group) $
+                        flip evalRandom (pureMT 1) (doNCrossovers n genomes (onePointCrossover 1))
+    , "two-point crossover" ~: do
+        let ones = replicate 8 1
+        let zeros = replicate 8 0
+        let genomes = [ones, zeros]
+        let n = 1000
+        assertEqual "exactly two crossover point" True $
+                    all (<=3) . map (length . group) $
+                        flip evalRandom (pureMT 1) (doNCrossovers n genomes (twoPointCrossover 1))
+    , "uniform crossover" ~: do
+        assertEqual "change all points"
+                    ([[0,0,0,0,0,0,0,0,0,0],[1,1,1,1,1,1,1,1,1,1]],[]) $
+                    flip evalRandom (pureMT 1) $
+                             (uniformCrossover 1) [replicate 10 1,replicate 10 (0::Int)]
+        assertEqual "change nothing"
+                    ([[1,1,1,1,1,1,1,1,1,1],[0,0,0,0,0,0,0,0,0,0]],[]) $
+                    flip evalRandom (pureMT 1) $
+                             (uniformCrossover 0) [replicate 10 1,replicate 10 (0::Int)]
+        let n = 1000
+        let mu = 0.5*n
+        let sigma = sqrt(n*0.5*(1-0.5))  -- normal approx to binomial distribution
+        let genomes = [ replicate (round n) 1
+                      , replicate (round n) 0]
+        let xover = uniformCrossover 0.5 :: CrossoverOp Double
+        let mkChildren = doNCrossovers 1000 genomes xover :: Rand [Genome Double]
+        let children = flip evalRandom (pureMT 1) mkChildren :: [Genome Double]
+        assertEqual "change approximately half" True $
+                    all (\s -> (s >= mu - 4*sigma && s <= mu + 4*sigma)) . map sum $
+                        children
     ]
