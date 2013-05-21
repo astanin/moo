179d580d39b85ca38a2ea35b8b382738187268a7
Sergey Astanin <s.astanin@gmail.com>
2013-05-21 17:25:11 +0200

    test withProbability

--- Tests/Internals/TestFundamentals.hs
+++ Tests/Internals/TestFundamentals.hs
@@ -2,10 +2,12 @@ module Tests.Internals.TestFundamentals where
 
 
 import Test.HUnit
+import System.Random.Mersenne.Pure64 (pureMT)
 
 
 import Moo.GeneticAlgorithm.Types
 import Moo.GeneticAlgorithm.Multiobjective.Types
+import Moo.GeneticAlgorithm.Random
 
 
 testFundamentals =
@@ -14,4 +16,11 @@ testFundamentals =
         assertEqual "raw genome" [True] $ takeGenome [True]
         assertEqual "phenotype" [True,True] $ takeGenome ([True,True], 42.0::Double)
         assertEqual "multiobjective phenotype" [False] $ takeGenome ([False], [42.0::Double])
+    , "withProbability" ~: do
+        assertEqual "probability 0" 42 $
+                    flip evalRandom (pureMT 1) $
+                    withProbability 0 (return . (+1)) 42
+        assertEqual "probability 1" 43 $
+                    flip evalRandom (pureMT 1) $
+                    withProbability 1 (return . (+1)) 42
     ]
